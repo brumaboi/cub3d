cde9f5ea64d00d15d26a227e4a2bf8559bb5191b
brumaboi <brumastefan8@gmail.com>
2025-02-16 22:28:20 +0100

    added structures I might need, inspired from my so_long + starting to do the parsing

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -13,6 +13,54 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <math.h>
+# include <mlx.h>
 
+typedef struct s_cub3d
+{
+    mlx_t       *mlx;
+    mlx_win_t   *win;
+    mlx_img_t   *img;
+    t_map       map;
+    t_player    player;
+}   t_cub3d;
+
+typedef struct s_player
+{
+    t_pos   pos;
+    int     direction;
+    int     turn_direction;
+    int     walk_direction;
+    double  rotation_angle;
+    double  walk_speed;
+    double  turn_speed;
+}   t_player;
+
+typedef struct s_pos
+{
+    int x;
+    int y;
+}   t_pos;
+
+typedef struct s_map
+{
+    char    **map;
+    int     rows;
+    int     columns;
+    int     player;
+    t_pos   player_pos;
+    int     initial_render;
+    char   *nord_texture;
+    char   *south_texture;
+    char   *west_texture;
+    char   *east_texture;
+    char   *floor_texture;
+    char   *ceiling_texture;
+    mlx_t  *mlx;
+}   t_map;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@ static int	check_format(char *path)
 	extension = ft_strrchr(path, '.');
 	if (extension)
 	{
-		if (!ft_strncmp(extension, ".ber", 4))
+		if (!ft_strncmp(extension, ".cub", 4))
 			return (1);
 	}
 	return (0);
@@ -33,12 +33,36 @@ int check_input(int argc, char **argv)
         return(printf("Error: Invalid file format\n"), 1);
     if (access(argv[1], F_OK) != 0)
         return(printf("Error: File does not exist\n"), 1);
-    check_if_valid_map(argv[1]);
+    return (0);
+}
+
+check_map(char *path)
+{
+    int fd;
+    char *line;
+    t_map map;
+
+    ft_bzero(&map, sizeof(t_map));
+    fd = open(path, O_RDONLY);
+    if (fd < 0)
+        return (printf("Error: Could not open file\n"), 1);
+    line = get_next_line(fd);
+    while (line > 0)
+    {
+        ///parsing logic
+        line = get_next_line(fd);
+    }
+    return (0);
 }
 
 int main(int argc, char **argv)
 {
+    t_cub3d cub;
+
+    ft_bzero(&cub, sizeof(t_cub3d));
     if (!check_input(agrc, argv))
+        return (1);
+    if (!check_map(argv[1], &cub))
         return (1);
     return (0);
 }
