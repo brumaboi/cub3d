25f26cd3129422efde60a0ad416d647cf90dea9e
brumaboi <brumastefan8@gmail.com>
2025-02-16 21:47:07 +0100

    started working on project, input validation

--- src/main.c
+++ src/main.c
@@ -12,7 +12,33 @@
 
 #include "../inc/cub3d.h"
 
+static int	check_format(char *path)
+{
+	char	*extension;
+
+	extension = ft_strrchr(path, '.');
+	if (extension)
+	{
+		if (!ft_strncmp(extension, ".ber", 4))
+			return (1);
+	}
+	return (0);
+}
+
+int check_input(int argc, char **argv)
+{
+    if (argc != 2)
+        return(printf("Error: Invalid number of arguments\n"), 1);
+    if (!check_format(argv[1]))
+        return(printf("Error: Invalid file format\n"), 1);
+    if (access(argv[1], F_OK) != 0)
+        return(printf("Error: File does not exist\n"), 1);
+    check_if_valid_map(argv[1]);
+}
+
 int main(int argc, char **argv)
 {
-
+    if (!check_input(agrc, argv))
+        return (1);
+    return (0);
 }
