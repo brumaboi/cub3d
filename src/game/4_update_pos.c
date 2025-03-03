7ed55e8c61e30a85b03b5274192ba4ad6eb9528d
Stefan-Andrei Bruma <sbruma@1-A-25.42heilbronn.de>
2025-03-03 13:20:13 +0100

    added function that updates camera plane and player direction(rotation)

--- src/game/4_update_pos.c
+++ src/game/4_update_pos.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   3_update_pos.c                                     :+:      :+:    :+:   */
+/*   4_update_pos.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbruma <sbruma@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 12:23:19 by sbruma            #+#    #+#             */
-/*   Updated: 2025/03/03 12:27:49 by sbruma           ###   ########.fr       */
+/*   Updated: 2025/03/03 13:18:24 by sbruma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,6 +92,46 @@ void update_right(t_cub3d *cub, double move_speed)
     }
 }
 
+void update_rotate_left(t_cub3d *cub)
+{
+    double old_dir_x;
+    double old_plane_x;
+
+    if (cub->player.input_rotate_left)
+    {
+        old_dir_x = cub->player.dir.x;
+        cub->player.dir.x = cub->player.dir.x * cos(cub->player.turn_speed)
+                           - cub->player.dir.y * sin(cub->player.turn_speed);
+        cub->player.dir.y = old_dir_x * sin(cub->player.turn_speed)
+                            + cub->player.dir.y * cos(cub->player.turn_speed);
+        old_plane_x = cub->player.plane.x;
+        cub->player.plane.x = cub->player.plane.x * cos(cub->player.turn_speed)
+                              - cub->player.plane.y * sin(cub->player.turn_speed);
+        cub->player.plane.y = old_plane_x * sin(cub->player.turn_speed)
+                                + cub->player.plane.y * cos(cub->player.turn_speed);
+    }
+}
+
+void update_rotate_right(t_cub3d *cub)
+{
+    double old_dir_x;
+    double old_plane_x;
+
+    if (cub->player.input_rotate_right)
+    {
+        old_dir_x = cub->player.dir.x;
+        cub->player.dir.x = cub->player.dir.x * cos(-cub->player.turn_speed)
+                           - cub->player.dir.y * sin(-cub->player.turn_speed);
+        cub->player.dir.y = old_dir_x * sin(-cub->player.turn_speed)
+                            + cub->player.dir.y * cos(-cub->player.turn_speed);
+        old_plane_x = cub->player.plane.x;
+        cub->player.plane.x = cub->player.plane.x * cos(-cub->player.turn_speed)
+                              - cub->player.plane.y * sin(-cub->player.turn_speed);
+        cub->player.plane.y = old_plane_x * sin(-cub->player.turn_speed)
+                                + cub->player.plane.y * cos(-cub->player.turn_speed);
+    }
+}
+
 void update_player(t_cub3d *cub)
 {
     double move_speed;
@@ -101,6 +141,6 @@ void update_player(t_cub3d *cub)
     update_backward(cub, move_speed);
     update_left(cub, move_speed);
     update_right(cub, move_speed);
-    // update_rotate_left(cub);
-    // update_rotate_right(cub);
+    update_rotate_left(cub);
+    update_rotate_right(cub);
 }
